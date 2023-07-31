d95eef066b80995e49b336a7a795e7bd9eb7db1c
0xLadoo <sendev@rage.trade>
2023-07-31 17:44:17 +0530

    feat: simulate order interface

--- src/interface.ts
+++ src/interface.ts
@@ -29,8 +29,11 @@ export type Market = {
   mode: Mode;
   longCollateral: Token["address"];
   shortCollateral: Token["address"];
-  indexOrIdentifier: Token["address"];
   supportedOrderTypes: Record<OrderType, Boolean>;
+} & MarketIdentifier;
+
+export type MarketIdentifier = {
+  indexOrIdentifier: string;
 };
 
 export type Position = {
@@ -40,23 +43,34 @@ export type Position = {
   // find equivalent on SNX
   averageEntryPrice: BigNumber;
   // verify accrued funding for SNX
-  cumulativeFunding: BigNumber;
+  cumulativeFunding?: BigNumber;
   // check for SNX
-  lastUpdatedAtTimestamp: number;
+  lastUpdatedAtTimestamp?: number;
 };
 
 export type ExtendedPosition = Position & {
-  unrealizedPnl: BigNumber;
+  unrealizedPnl?: BigNumber;
   liqudationPrice: BigNumber;
   otherFees?: BigNumber;
+  fee?: BigNumber;
+  leverage?: BigNumber;
+  status?: number;
+  priceImpact?: BigNumber;
+  exceedsPriceProtection?: boolean;
+  sizeDelta?: BigNumber;
+  skewAdjustedPrice?: BigNumber;
+};
+
+export type CollateralData = {
+  inputCollateral: Token;
+  inputCollateralAmount: BigNumber;
 };
 
 export type Order = {
   // check for GMX how frontend will know if it MI or MD
   type: OrderType;
   direction: OrderDirection;
-  inputCollateral: Token;
-  inputCollateralAmount: BigNumber;
+
   // in SNX cannot update size and collateral delta in same call
   sizeDelta: BigNumber;
   isTriggerOrder: Boolean;
@@ -67,7 +81,7 @@ export type Order = {
         triggerAboveThreshold: Boolean;
       }
     | undefined;
-};
+} & CollateralData;
 
 // at any point in time, there is only one delayed order per market
 export type OrderIdentifier = BigNumberish;
@@ -75,41 +89,63 @@ export type OrderIdentifier = BigNumberish;
 export interface IExchange {
   // something to indicate when setup should be called
   setup(signer: Signer): Promise<void>;
+
   supportedNetworks(): readonly Network[];
+
   supportedMarkets(network: Network): Promise<readonly Market[]>;
+
   createOrder(
     signer: Signer,
     market: Market,
     order: Order
   ): Promise<UnsignedTransaction>;
+
   updateOrder(
     singer: Signer,
     market: Market,
     orderIdentifier: OrderIdentifier,
     updatedOrder: Partial<Order>
   ): Promise<UnsignedTransaction>;
+
   cancelOrder(
     signer: Signer,
     market: Market,
     orderIdentifier: OrderIdentifier
   ): Promise<UnsignedTransaction>;
+
   getOrder(
     orderIdentifier: OrderIdentifier
   ): Promise<Array<Order & OrderAction & OrderIdentifier>>;
+
   getOrders(
     user: string
   ): Promise<Array<Order & OrderAction & OrderIdentifier>>;
+
   getOrders(
     user: string,
     market: Market["indexOrIdentifier"]
   ): Promise<Array<Order & OrderAction & OrderIdentifier>>;
+
   getPosition(
     positionIdentifier: Position["indexOrIdentifier"]
   ): Promise<Position>;
+
   getPositions(user: string): Promise<Position[]>;
+
   getPositions(
     user: string,
     market: Market["indexOrIdentifier"]
   ): Promise<Position[]>;
+
   getExtendedPositions(positions: Position[]): Promise<ExtendedPosition[]>;
+
+  getIdleMargins(
+    user: string
+  ): Promise<Array<MarketIdentifier & CollateralData>>;
+
+  getTradePreview(
+    signer: Signer,
+    market: Market,
+    order: Order
+  ): Promise<ExtendedPosition>;
 }
