e3b07da5096a24dcf37cdef17f5c62a80f07361c
0xDosa <90966498+0xDosa@users.noreply.github.com>
2023-11-08 17:23:33 +0530

    feat: added market encode and decode helper

---
+++ src/common/markets.ts
@@ -0,0 +1,11 @@
+function decodeMarketId(marketId: string) {
+    const [chainId, protocolId, protocolMarketId] = marketId.split('-');
+    return {
+        chainId,
+        protocolId,
+        protocolMarketId
+    }
+}
+function encodeMarketId(chainId: string, protocolId: string, protocolMarketId: string) {
+    return `${chainId}-${protocolId}-${protocolMarketId}`;
+}
