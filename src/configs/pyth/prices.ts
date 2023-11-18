7496e1804a85306f3e8720ae32af571dafe8eb31
protodev-rage <protodev@rage.trade>
2023-11-18 15:25:08 +0530

    fix: pyth price feed x2

--- src/configs/pyth/prices.ts
+++ src/configs/pyth/prices.ts
@@ -4,18 +4,46 @@ import { NumberDecimal } from '../../interface'
 // Assuming you're working in a browser environment that supports fetch and ReadableStream
 const streamingUrl = 'https://benchmarks.pyth.network/v1/shims/tradingview/streaming'
 
+interface Bar {
+  time: number
+  open: number
+  high: number
+  low: number
+  close: number
+  volume?: number
+}
+
+interface SubscriptionInfo {
+  subscriberUID: string
+  resolution: string
+  lastDailyBar: Bar
+  handlers: [
+    {
+      id: string
+      callback: (bar: Bar) => void
+    }
+  ]
+}
+
+const channelToSubscription = new Map<string, SubscriptionInfo>()
+
 type PricesMap = Record<string, NumberDecimal | null>
 
 let prices: PricesMap = {}
 
-function handleStreamingData(data: { id: string; p: number }) {
-  const { id, p } = data
+function getNextDailyBarTime(barTime: number) {
+  const date = new Date(barTime * 1000)
+  date.setDate(date.getDate() + 1)
+  return date.getTime() / 1000
+}
+
+function handleStreamingData(data: { id: string; p: number; t: number }) {
+  const { id, p, t } = data
+
   try {
     const symbolInfo = id.split('.')[1]
     const symbol = symbolInfo.split('/')[0]
     const symbolBase = symbolInfo.split('/')[1]
-
-    // console.log({ id, symbolInfo, symbol, symbolBase, p });
 
     if (symbolBase === 'USD') {
       prices[symbol] = {
@@ -27,6 +55,47 @@ function handleStreamingData(data: { id: string; p: number }) {
   } catch (e) {
     // console.error(`[stream] Error parsing streaming data:`);
   }
+
+  const tradePrice = p
+  const tradeTime = t * 1000 // Multiplying by 1000 to get milliseconds
+
+  const channelString = id.split('.')[1].replace('/', '-')
+  const subscriptionItem = channelToSubscription.get(channelString)
+  // console.log({ id, channelString, channelToSubscription, subscriptionItem })
+
+  if (!subscriptionItem) {
+    return
+  }
+
+  const lastDailyBar = subscriptionItem.lastDailyBar
+  const nextDailyBarTime = getNextDailyBarTime(lastDailyBar.time)
+
+  let bar: Bar
+
+  if (tradeTime >= nextDailyBarTime) {
+    bar = {
+      time: nextDailyBarTime,
+      open: tradePrice,
+      high: tradePrice,
+      low: tradePrice,
+      close: tradePrice
+    }
+    // console.log('[stream] Generate new bar', bar)
+  } else {
+    bar = {
+      ...lastDailyBar,
+      high: Math.max(lastDailyBar.high, tradePrice),
+      low: Math.min(lastDailyBar.low, tradePrice),
+      close: tradePrice
+    }
+    // console.log('[stream] Update the latest bar by price', tradePrice)
+  }
+
+  subscriptionItem.lastDailyBar = bar
+
+  // Send data to every subscriber of that symbol
+  subscriptionItem.handlers.forEach((handler) => handler.callback(bar))
+  channelToSubscription.set(channelString, subscriptionItem)
 }
 
 export function startStreaming(retries = 3, delay = 3000) {
@@ -135,4 +204,51 @@ export function getTokenPriceD(token: string, decimals: number) {
   }
 }
 
-// startStreaming()
+export function subscribeOnStream(
+  symbolInfo: { ticker: string },
+  resolution: string,
+  onRealtimeCallback: (bar: Bar) => void,
+  subscriberUID: string,
+  _: () => void,
+  lastDailyBar: Bar
+) {
+  const channelString = symbolInfo.ticker
+  const handler = {
+    id: subscriberUID,
+    callback: onRealtimeCallback
+  }
+
+  let subscriptionItem = channelToSubscription.get(channelString)
+
+  subscriptionItem = {
+    subscriberUID,
+    resolution,
+    lastDailyBar,
+    handlers: [handler]
+  }
+  channelToSubscription.set(channelString, subscriptionItem)
+  console.log('[subscribeBars]: Subscribe to streaming. Channel:', channelString)
+}
+
+export function unsubscribeFromStream(subscriberUID: string) {
+  // Find a subscription with id === subscriberUID
+  for (const channelString of channelToSubscription.keys()) {
+    const subscriptionItem = channelToSubscription.get(channelString)
+
+    if (!subscriptionItem) {
+      console.log('[unsubscribeBars]: Subscription item not found')
+      break
+    }
+
+    const handlerIndex = subscriptionItem.handlers.findIndex((handler) => handler.id === subscriberUID)
+
+    if (handlerIndex !== -1) {
+      // Unsubscribe from the channel if it is the last handler
+      console.log('[unsubscribeBars]: Unsubscribe from streaming. Channel:', channelString)
+      channelToSubscription.delete(channelString)
+      break
+    }
+  }
+}
+
+startStreaming()
