8d5452676743666fd28efc27e8a271928df3700e
0xLadoo <sendev@rage.trade>
2024-01-20 12:12:05 +0530

    chore: buttonHeadings fix

--- src/common/buttonHeadings.ts
+++ src/common/buttonHeadings.ts
@@ -4,7 +4,6 @@ export const EMPTY_DESC = ''
 export const GMXV1_ENABLE_ORDERBOOK_H = 'GMXv1: Enable Limit Orders'
 export const GMXV1_ENABLE_POSITION_ROUTER_H = 'GMXv1: Enable Market Orders'
 export const GMX_SET_REFERRAL_CODE_H = 'GMX: Set Referral Code'
-export const TOKEN_APPROVAL_H = 'Approve Collateral'
 export const UPDATE_ORDER_H = 'Edit Price'
 export const CANCEL_ORDER_H = 'Cancel Order'
 export const UPDATE_DEPOSIT_H = 'Add Collateral'
@@ -24,7 +23,7 @@ export function getClosePositionHeading(protocolId: ProtocolId, marketSymbol: st
 }
 
 export function getApproveTokenHeading(tokenSymbol: string) {
-  return `${TOKEN_APPROVAL_H} ${_getTokenSymbol(tokenSymbol)}`
+  return `Approve ${_getTokenSymbol(tokenSymbol)} Collateral`
 }
 
 function _getTPSLString(type: CloseOrderType) {
