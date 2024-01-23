3e38659ce85249a928aa94a66903e45e43866aa9
protodev-rage <protodev@rage.trade>
2024-01-24 01:06:02 +0530

    feat: add trigger populate logic

--- src/configs/hyperliquid/helper.ts
+++ src/configs/hyperliquid/helper.ts
@@ -1,5 +1,7 @@
-import { Level } from './api/types'
+import { roundedPrice } from './api/client'
+import { Level, ModifyRequest } from './api/types'
 import { FixedNumber, abs, bipsDiff } from '../../common/fixedNumber'
+import { OrderType, TriggerData } from '../../interfaces/V1/IRouterAdapterBaseV1'
 
 export function indexBasisSlippage(val: Level[], slippageBp: string): number {
   const val0p = FixedNumber.fromString(val[0].px)
@@ -17,4 +19,56 @@ export function indexBasisSlippage(val: Level[], slippageBp: string): number {
 
 export function hlMarketIdToCoin(marketId: string): string {
   return marketId.split('-')[2]
+}
+
+export function validateTrigger(isBuy: boolean, midPrice: number, triggerPrice: number, isStop: boolean): boolean {
+  return (
+    (isBuy && (isStop ? triggerPrice > midPrice : triggerPrice < midPrice)) ||
+    (!isBuy && (isStop ? triggerPrice < midPrice : triggerPrice > midPrice))
+  )
+}
+
+export function popuplateTrigger(
+  isBuy: boolean,
+  midPrice: number,
+  orderType: OrderType,
+  triggerData: TriggerData
+): {
+  orderData: ModifyRequest['order']['order_type']
+  limitPrice: ModifyRequest['order']['limit_px']
+} {
+  if (orderType == 'MARKET' || orderType == 'LIMIT') throw new Error('trigger used with wrong order type')
+
+  if (!triggerData.triggerActivatePrice) throw new Error('trigger price required')
+
+  const isStop = orderType == 'STOP_LOSS' || orderType == 'STOP_LOSS_LIMIT'
+
+  const triggerPrice = roundedPrice(Number(triggerData.triggerPrice._value))
+  const triggerActivatePrice = roundedPrice(Number(triggerData.triggerActivatePrice._value))
+
+  // this check is required, otherwise it is not considered resting order and executes immidiately
+  if (!validateTrigger(isBuy, midPrice, triggerActivatePrice, isStop))
+    throw new Error('trigger orderType, current price & trigger activation price are not compatible')
+
+  if (isStop) {
+    const orderData: ModifyRequest['order']['order_type'] = {
+      trigger: {
+        triggerPx: triggerActivatePrice,
+        isMarket: !orderType.includes('LIMIT'),
+        tpsl: 'sl'
+      }
+    }
+
+    return { orderData, limitPrice: triggerPrice }
+  } else {
+    const orderData: ModifyRequest['order']['order_type'] = {
+      trigger: {
+        triggerPx: triggerActivatePrice,
+        isMarket: !orderType.includes('LIMIT'),
+        tpsl: 'tp'
+      }
+    }
+
+    return { orderData, limitPrice: triggerPrice }
+  }
 }
