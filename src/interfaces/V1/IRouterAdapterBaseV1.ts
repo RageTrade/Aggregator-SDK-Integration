9bc873401ba6740d0b09494c581994bea898b7be
0xLadoo <sendev@rage.trade>
2024-01-23 13:38:42 +0530

    chore: remove duplicate

--- src/interfaces/V1/IRouterAdapterBaseV1.ts
+++ src/interfaces/V1/IRouterAdapterBaseV1.ts
@@ -446,8 +446,6 @@ export interface IRouterAdapterBaseV1 {
 
   getMarketState(wallet: string, marketIds: Market['marketId'][], opts?: ApiOpts): Promise<MarketState[]>
 
-  getMarketState(wallet: string, marketIds: Market['marketId'][], opts?: ApiOpts): Promise<MarketState[]>
-
   //Helper
   getAmountInfoType(): AmountInfoInToken
 }
