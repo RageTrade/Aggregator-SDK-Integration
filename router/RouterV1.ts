3cd16251640d76a4c20ea6d76e1b7ddefaae2f75
protodev-rage <protodev@rage.trade>
2024-01-30 20:41:31 +0530

    fix: remove market check in router

--- router/RouterV1.ts
+++ router/RouterV1.ts
@@ -60,8 +60,7 @@ export default class RouterV1 implements IRouterV1 {
     const promises: Promise<ActionParam[]>[] = []
 
     params.forEach((param) => {
-      if (!param.market) throw new Error('marketId required for deposit')
-      const adapter = this._checkAndGetAdapter(param.market)
+      const adapter = this.adapters[param.protocol]
       promises.push(adapter.deposit([param]))
     })
 
