028097c5d4a9de63fce4a5bea2dbcb647c264f7a
protodev-rage <protodev@rage.trade>
2024-02-15 04:01:50 +0530

    ref: modular execute & add api call param handling

--- test/execute.ts
+++ test/execute.ts
@@ -9,6 +9,8 @@ export async function execute(wallet: WalletClient, agentWallet: WalletClient, e
 
   for (const payload of executionPayload) {
     if (isUnsignedTxWithMetadata(payload)) {
+      console.log('UNSIGNED TX WITH METADATA')
+
       const tx = await wallet.sendTransaction({
         chain: wallet.chain,
         account: wallet.account!,
@@ -26,7 +28,14 @@ export async function execute(wallet: WalletClient, agentWallet: WalletClient, e
       continue
     }
 
-    if (!isRequestSignerFn(payload)) continue
+    if (!isRequestSignerFn(payload)) {
+      console.log('API PARAMS')
+      ret.push(...(await handleRequest(payload.apiArgs, payload.heading)))
+
+      continue
+    }
+
+    console.log('REQUEST SIGNER FN')
 
     // is submitting
     const fetchPayload = await payload.fn(
@@ -36,31 +45,39 @@ export async function execute(wallet: WalletClient, agentWallet: WalletClient, e
 
     if (!fetchPayload) continue
 
-    // is executing
-    const res = await fetch(...fetchPayload).catch((e) => {
-      // failure
-      console.log(payload.heading, 'failed (catch block)')
-      console.dir(e.body, { depth: 6 })
-    })
+    ret.push(...(await handleRequest(fetchPayload, payload.heading)))
+  }
 
-    if (!res) throw new Error('res not found')
+  return ret
+}
 
-    if (res.ok) {
-      // is success
-      const data = await res.json()
-      ret.push(data)
+async function handleRequest(args: Parameters<typeof fetch>, heading: string) {
+  // is executing
+  const ret = []
 
-      console.log(payload.heading, 'success')
-      console.dir(data, { depth: 6 })
-    } else {
-      // failure
-      console.log(payload.heading, 'failed (not ok code)')
+  const res = await fetch(...args).catch((e) => {
+    // failure
+    console.log(heading, 'failed (catch block)')
+    console.dir(e.body, { depth: 6 })
+  })
 
-      try {
-        console.dir(await res.json(), { depth: 6 })
-      } catch {
-        console.dir(res, { depth: 6 })
-      }
+  if (!res) throw new Error('res not found')
+
+  if (res.ok) {
+    // is success
+    const data = await res.json()
+    ret.push(data)
+
+    console.log(heading, 'success')
+    console.dir(data, { depth: 6 })
+  } else {
+    // failure
+    console.log(heading, 'failed (not ok code)')
+
+    try {
+      console.dir(await res.json(), { depth: 6 })
+    } catch {
+      console.dir(res, { depth: 6 })
     }
   }
 
