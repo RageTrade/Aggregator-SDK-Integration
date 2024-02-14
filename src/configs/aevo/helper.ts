8a81d47610553f0ef9aa3c7d2a8131ef14f75f34
protodev-rage <protodev@rage.trade>
2024-02-14 16:28:36 +0530

    feat: helpers for conversion

--- src/configs/aevo/helper.ts
+++ src/configs/aevo/helper.ts
@@ -1,5 +1,5 @@
 import { FixedNumber, abs, bipsDiff } from '../../common/fixedNumber'
-
+import { HL_LEV_OUT_OF_BOUNDS } from '../hyperliquid/hlErrors'
 export function aevoMarketIdToAsset(marketId: string): string {
   return marketId.split('-')[2]
 }
@@ -20,4 +20,32 @@ export function aevoIndexBasisSlippage(val: Array<string[]>, slippageBp: string)
 
 export function aevoInstrumentNameToAsset(instrument_name: string): string {
   return instrument_name.split('-')[0]
+}
+
+export function to6Decimals(input: number | string): number {
+  const inputNumber = typeof input === 'string' ? parseFloat(input) : input
+  const result = inputNumber * 1e6
+  return Number(result.toFixed(6))
+}
+
+export function to6DecimalsFN(input: FixedNumber): number {
+  const inputNumber = Number(input._value)
+  const result = inputNumber * 1e6
+  return Number(result.toFixed(6))
+}
+
+export function getReqdLeverage(sizeDelta: number, marginDelta: number, price: number) {
+  if (marginDelta <= 0 || marginDelta > 20) throw new Error(HL_LEV_OUT_OF_BOUNDS)
+
+  return Math.round((sizeDelta * price) / marginDelta)
+}
+
+export function getReqdLeverageFN(sizeDelta: FixedNumber, marginDelta: FixedNumber, price: FixedNumber) {
+  const px = Number(price._value)
+  const sd = Number(sizeDelta._value)
+  const md = Number(marginDelta._value)
+
+  if (md <= 0 || md > 20) throw new Error(HL_LEV_OUT_OF_BOUNDS)
+
+  return Math.round((sd * px) / md)
 }
